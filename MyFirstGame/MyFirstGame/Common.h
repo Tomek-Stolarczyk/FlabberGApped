740840c979fb08d93af983e1e3ab505171012cef
Tomek <tomek.stolarczyk9@gmail.com>
2016-12-06 17:17:24 -0500

    add vertex and index typedefs

--- MyFirstGame/MyFirstGame/Common.h
+++ MyFirstGame/MyFirstGame/Common.h
@@ -2,13 +2,23 @@
 
 #include <DirectXMath.h>
 
-typedef struct
+struct VERTEX
 {
     DirectX::XMFLOAT3 pos;
     DirectX::XMFLOAT2 texCoord;
-} VERTEX, *pVertex;
+
+    VERTEX(float v1, float v2, float v3) :
+        pos(v1, v2, v3),
+        texCoord(0,0)
+    {
+
+    }
+
+};
 
 struct cbPerObject
 {
     DirectX::XMMATRIX  WVP;
 };
+
+typedef int INDEX;
